aba92bc0c97e3f7e6fc526b7ab58324eeb04a1fd
SeriousSeba <prykstefan@gmail.com>
2020-05-24 21:21:38 +0200

    Further changes

--- api/rest/host_blocker.py
+++ api/rest/host_blocker.py
@@ -12,17 +12,21 @@ class HostBlocker:
     def __init__(self):
         self.iptables = '/sbin/iptables'
 
+    def _validate_host(self, host):
+        if not self.IP_REGEX.match(host):
+            raise ValueError('bad host format - should be IPv4')
+
     def _save_changes(self):
         subprocess.run([self.iptables+'-save'], check=True)
 
     def block_host(self, host_ip: str):
         self._validate_host(host_ip)
-        self.add_host_to_ip_tables(host_ip)
-
-    def unblock_host(self, host_ip: str):
-        self._validate_host(host_ip)
-        logger.info(f'Unblocking host {host_ip}')
-        self.remove_host_from_ip_tables(host_ip)
+        logger.info(f'Blocking host {host_ip}')
+        command_ip_out = [self.iptables, '-A', 'OUTPUT', '-d', host_ip, '-j', 'REJECT']
+        command_ip_in = [self.iptables, '-A', 'INPUT', '-s', host_ip, '-j', 'DROP']
+        subprocess.run(command_ip_in, check=True)
+        subprocess.run(command_ip_out, check=True)
+        self._save_changes()
 
     def _find_rule_id_by_name(self, table: str, host_ip: str) -> Optional[int]:
         table = table.decode()
@@ -33,20 +37,9 @@ class HostBlocker:
             if host_ip in line and ('REJECT' in line or 'DROP' in line):
                 return line[0]
 
-    def _validate_host(self, host):
-        if not self.IP_REGEX.match(host):
-            logger.error(f'Bad host format - should be IPv4 {host}')
-
-
-    def add_host_to_ip_tables(self, host_ip: str):
-        logger.info(f'Blocking host {host_ip}')
-        command_ip_out = [self.iptables, '-A', 'OUTPUT', '-d', host_ip, '-j', 'REJECT']
-        command_ip_in = [self.iptables, '-A', 'INPUT', '-s', host_ip, '-j', 'DROP']
-        subprocess.run(command_ip_in, check=True)
-        subprocess.run(command_ip_out, check=True)
-        self._save_changes()
-
-    def remove_host_from_ip_tables(self, host_ip: str):
+    def unblock_host(self, host_ip: str):
+        self._validate_host(host_ip)
+        logger.info(f'Unblocking host {host_ip}')
         output_table = subprocess.check_output([self.iptables, '-L', 'OUTPUT', '-n', '--line-numbers'])
         input_table = subprocess.check_output([self.iptables, '-L', 'INPUT', '-n', '--line-numbers'])
         output_id = self._find_rule_id_by_name(output_table, host_ip)
